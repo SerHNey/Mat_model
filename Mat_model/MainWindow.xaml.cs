82b9aa8987f396403ab8fab541cb8337c979b81c
SerHNow <naumov.sergey2003@gmail.com>
2022-05-04 20:23:19 +0300

    1

--- Mat_model/MainWindow.xaml.cs
+++ Mat_model/MainWindow.xaml.cs
@@ -37,69 +37,6 @@ namespace Mat_model
             }
 
         }
-        private void Update_vectors(int b)//получает числа введенные в size_r и size_c
-        {
-            //получаем кол-во колонок 
-            for (int i = 0; i < b; i++)//создаем кол-во колонок и текстовых блоков равное чилу введенному в size_r
-            {
-                var text_box = new TextBox();//создаем экземпляр объекта TextBox
-                var Col = new ColumnDefinition();//создаем экземпляр объекта ColumnDefinition
-
-                text_box.Foreground = new SolidColorBrush(Colors.Black);//цвет текста
-                text_box.Background = new SolidColorBrush(Colors.White);//цвет фона
-                text_box.TextAlignment = TextAlignment.Center;//выравнивание по центру 
-                text_box.Style = (Style)size_c.FindResource("TextBoxStyle1");//применяем стиль блока (там просто цвет выделения блока на белый поменял)
-                text_box.Name = $"Power_{i}";//присваеваем блоку имя, чтобы потом по имени получать из него значение
-                text_box.FontSize = 16;
-                try
-                {
-                    this.RegisterName(text_box.Name, text_box);//пытаемся зарегистрировать имя
-                }
-                catch//если такое имя есть, удаляем его и еще раз регистрируем
-                {
-                    this.UnregisterName(text_box.Name);
-                    this.RegisterName(text_box.Name, text_box);
-                }
-                //добавляем метод еоторый будет вызываться при изменениии значения в блоке 
-                if (i != 0)
-                {
-                    text_box.BorderBrush = new SolidColorBrush(Colors.White);//цвет контура
-                    text_box.BorderThickness = new Thickness(2, 0, 0, 0);//ширина контура (справо,верх,слево,низ)
-                }
-                else text_box.BorderThickness = new Thickness(0);
-                //"засовываем" получившийся блок в сетку power_vector
-                Grid.SetColumn(text_box, i);//выбираем колонку для элемента
-            }
-            //тут тоже самое только для другой 
-            for (int i = 0; i < b; i++)//создаем кол-во колонок и текстовых блоков равное чилу введенному в size_с
-            {
-                var text_box = new TextBox();
-                var Col = new ColumnDefinition();
-
-                text_box.Foreground = new SolidColorBrush(Colors.White);
-                text_box.Background = new SolidColorBrush(Colors.Black);
-                text_box.TextAlignment = TextAlignment.Center;
-                text_box.Style = (Style)size_c.FindResource("TextBoxStyle1");
-                text_box.Name = $"Demand_{i}";
-                text_box.FontSize = 16;
-                try
-                {
-                    this.RegisterName(text_box.Name, text_box);
-                }
-                catch
-                {
-                    this.UnregisterName(text_box.Name);
-                    this.RegisterName(text_box.Name, text_box);
-                }
-
-                if (i != 0)
-                {
-                    text_box.BorderBrush = new SolidColorBrush(Colors.White);
-                    text_box.BorderThickness = new Thickness(2, 0, 0, 0);
-                }
-                else text_box.BorderThickness = new Thickness(0);
-            }
-        }
         private void Update_array(int b)
         {
             //получаем кол-во столбцов и строк в матрице чтобы удалить их
@@ -174,14 +111,12 @@ namespace Mat_model
                 }
             }
         }
-        
 
         private void size_c_TextChanged(object sender, TextChangedEventArgs e)
         {
             if (!Check_input(size_c.Text)) size_c.Text = ""; //Если метод, проверяющий буква вводится или нет, возвращает false строка становится пустой 
             if (size_c.Text != "")
             {
-                Update_vectors(Convert.ToInt32(size_c.Text));//создаем в сетках для векторов нужное кол-во колонок и текстовых блоков
                 Update_array(Convert.ToInt32(size_c.Text));//создаем матрицу
             }
 
